f8676b01bc49da942dff860cdb3604181916e979
liuto <liutao@shopee.com>
2025-10-24 16:55:13 +0800

    rice dataset test

--- data/rice_dataset.py
+++ data/rice_dataset.py
@@ -12,77 +12,58 @@ from basicsr.utils.registry import DATASET_REGISTRY
 
 @DATASET_REGISTRY.register()
 class RICEDataset(data.Dataset):
-    """Example dataset.
-
-    1. Read GT image
-    2. Generate LQ (Low Quality) image with cv2 bicubic downsampling and JPEG compression
-
-    Args:
-        opt (dict): Config for train datasets. It contains the following keys:
-            dataroot_gt (str): Data root path for gt.
-            io_backend (dict): IO backend type and other kwarg.
-            gt_size (int): Cropped patched size for gt patches.
-            use_flip (bool): Use horizontal flips.
-            use_rot (bool): Use rotation (use vertical flip and transposing h
-                and w for implementation).
-
-            scale (bool): Scale, which will be added automatically.
-            phase (str): 'train' or 'val'.
-    """
+    """RICE 云雾去除数据集"""
 
     def __init__(self, opt):
         super(RICEDataset, self).__init__()
         self.opt = opt
-        # file client (io backend)
         self.file_client = None
         self.io_backend_opt = opt['io_backend']
         self.mean = opt['mean'] if 'mean' in opt else None
         self.std = opt['std'] if 'std' in opt else None
 
-        self.gt_folder = opt['dataroot_gt']
-        # it now only supports folder mode, for other modes such as lmdb and meta_info file, please see:
-        # https://github.com/xinntao/BasicSR/blob/master/basicsr/data/
-        self.paths = [os.path.join(self.gt_folder, v) for v in list(scandir(self.gt_folder))]
+        # 修改：分别指定清晰图片和云雾图片的路径
+        self.gt_folder = opt['dataroot_gt']  # 清晰图片（无云雾）
+        self.lq_folder = opt['dataroot_lq']  # 云雾遮挡图片
+
+        # 获取对应的文件对
+        self.gt_paths = [os.path.join(self.gt_folder, v) for v in list(scandir(self.gt_folder))]
+        self.lq_paths = [os.path.join(self.lq_folder, v) for v in list(scandir(self.lq_folder))]
+
+        # 确保文件对应（根据文件名匹配）
+        assert len(self.gt_paths) == len(self.lq_paths)
 
     def __getitem__(self, index):
         if self.file_client is None:
             self.file_client = FileClient(self.io_backend_opt.pop('type'), **self.io_backend_opt)
 
-        scale = self.opt['scale']
-
-        # Load gt images. Dimension order: HWC; channel order: BGR;
-        # image range: [0, 1], float32.
-        gt_path = self.paths[index]
+        # 加载清晰图片 (GT)
+        gt_path = self.gt_paths[index]
         img_bytes = self.file_client.get(gt_path, 'gt')
         img_gt = imfrombytes(img_bytes, float32=True)
-        img_gt = mod_crop(img_gt, scale)
 
-        # generate lq image
-        # downsample
-        h, w = img_gt.shape[0:2]
-        img_lq = cv2.resize(img_gt, (w // scale, h // scale), interpolation=cv2.INTER_CUBIC)
-        # add JPEG compression
-        img_lq = add_jpg_compression(img_lq, quality=70)
+        # 加载云雾图片 (LQ) - 修改点：直接读取云雾图像而不是生成
+        lq_path = self.lq_paths[index]
+        lq_bytes = self.file_client.get(lq_path, 'lq')
+        img_lq = imfrombytes(lq_bytes, float32=True)
 
-        # augmentation for training
+        # 数据增强
         if self.opt['phase'] == 'train':
             gt_size = self.opt['gt_size']
-            # random crop
-            img_gt, img_lq = paired_random_crop(img_gt, img_lq, gt_size, scale, gt_path)
-            # flip, rotation
+            # 随机裁剪
+            img_gt, img_lq = paired_random_crop(img_gt, img_lq, gt_size, scale=1, gt_path=gt_path)
+            # 翻转旋转
             img_gt, img_lq = augment([img_gt, img_lq], self.opt['use_flip'], self.opt['use_rot'])
 
-        # BGR to RGB, HWC to CHW, numpy to tensor
+        # 格式转换
         img_gt, img_lq = img2tensor([img_gt, img_lq], bgr2rgb=True, float32=True)
 
-        img_lq = torch.clamp((img_lq * 255.0).round(), 0, 255) / 255.
-
-        # normalize
+        # 标准化
         if self.mean is not None or self.std is not None:
             normalize(img_lq, self.mean, self.std, inplace=True)
             normalize(img_gt, self.mean, self.std, inplace=True)
 
-        return {'lq': img_lq, 'gt': img_gt, 'lq_path': gt_path, 'gt_path': gt_path}
+        return {'lq': img_lq, 'gt': img_gt, 'lq_path': lq_path, 'gt_path': gt_path}
 
     def __len__(self):
-        return len(self.paths)
+        return len(self.gt_paths)
